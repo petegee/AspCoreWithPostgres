68c8811101ed4d9e42077f9864370d645e872566
Peter Glassey [DATACOM] <PeterGla@datacom.co.nz>
2017-07-17 15:53:59 +1200

    Add project files.

---
+++ BlogPost.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AspCoreWithPostgres
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset Date { get; set; }
+    }
+}
---
+++ Controllers/BlogPostController.cs
@@ -0,0 +1,51 @@
+﻿using Marten;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AspCoreWithPostgres.Controllers
+{
+    [Route("/posts")]
+    public class BlogPostController
+    {
+        private readonly IDocumentStore documentStore;
+
+        public BlogPostController(IDocumentStore documentStore)
+        {
+            this.documentStore = documentStore;
+        }
+
+        [HttpGet]
+        public IEnumerable<BlogPost> Get()
+        {
+            using (var session = documentStore.QuerySession())
+            {
+                return session.Query<BlogPost>();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public BlogPost Get(int id)
+        {
+            using (var session = documentStore.QuerySession())
+            {
+                return session
+                    .Query<BlogPost>()
+                    .Where(post => post.Id == id)
+                    .FirstOrDefault();
+
+            }
+        }
+
+        [HttpPost]
+        public BlogPost Create([FromBody]BlogPost post)
+        {
+            using (var session = documentStore.LightweightSession())
+            {
+                session.Store(post);
+                session.SaveChanges();
+                return post;
+            }
+        }
+    }
+}
