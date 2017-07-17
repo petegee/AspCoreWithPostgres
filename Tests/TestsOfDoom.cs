01318d4d95ed9e25cd411082ccc6f2731a89294e
DATACOM-NZ\PeterGla <petergla@datacom.co.nz>
2017-07-17 16:06:25 +1200

    put web app in its own directory

---
+++ Tests/TestsOfDoom.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestsOfDoom
+    {
+        [Test]
+        public void TestThatShitIsNotWhack()
+        {
+            Assert.Pass("Your shit is NOT whack!");
+        }
+    }
+}
